5b10a7ff6874f942ec3f4a76a062e267782edb5c
tibrunaoliveira <ti.brunaoliveira@gmail.com>
2026-05-20 12:15:00 -0300

    Create PlantaRepository.java

---
+++ src/main/java/com/raiztech/raiztechapi/repository/PlantaRepository.java
@@ -0,0 +1,8 @@
+package com.raiztech.raiztech_api.repository;
+
+import com.raiztech.raiztech_api.entity.Planta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlantaRepository extends JpaRepository<Planta, Long> {
+
+}
