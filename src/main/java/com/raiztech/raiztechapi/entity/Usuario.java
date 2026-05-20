e5cc1da4b6d7cb39891ff129630c1f9dcd14f716
tibrunaoliveira <ti.brunaoliveira@gmail.com>
2026-05-20 12:13:15 -0300

    Create Usuario.java

---
+++ src/main/java/com/raiztech/raiztechapi/entity/Usuario.java
@@ -0,0 +1,49 @@
+package com.raiztech.raiztech_api.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+    public Usuario() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
