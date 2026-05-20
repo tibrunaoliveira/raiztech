d465db1423f5d8babc87c9276f9e675a81cfedf8
tibrunaoliveira <ti.brunaoliveira@gmail.com>
2026-05-20 12:14:29 -0300

    Create LeituraSensorRepository.java

---
+++ src/main/java/com/raiztech/raiztechapi/repository/LeituraSensorRepository.java
@@ -0,0 +1,8 @@
+package com.raiztech.raiztech_api.repository;
+
+import com.raiztech.raiztech_api.entity.LeituraSensor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LeituraSensorRepository extends JpaRepository<LeituraSensor, Long> {
+
+}
