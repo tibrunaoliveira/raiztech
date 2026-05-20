2f034f19e5d4bde92279031568e62c3f0dd34421
tibrunaoliveira <ti.brunaoliveira@gmail.com>
2026-05-20 12:14:01 -0300

    Create IrrigacaoRepository.java

---
+++ src/main/java/com/raiztech/raiztechapi/repository/IrrigacaoRepository.java
@@ -0,0 +1,8 @@
+package com.raiztech.raiztech_api.repository;
+
+import com.raiztech.raiztech_api.entity.Irrigacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IrrigacaoRepository extends JpaRepository<Irrigacao, Long> {
+
+}
