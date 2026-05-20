71f0697f00b51e07b06356fa1b3e2a92f5a19cf6
tibrunaoliveira <ti.brunaoliveira@gmail.com>
2026-05-20 12:15:48 -0300

    Create RaiztechApiApplication.java

---
+++ src/main/java/com/raiztech/raiztechapi/RaiztechApiApplication.java
@@ -0,0 +1,13 @@
+package com.raiztech.raiztech_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaiztechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaiztechApiApplication.class, args);
+	}
+
+}
